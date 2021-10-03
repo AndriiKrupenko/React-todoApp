2fc66aafdaaff895149b7cbe917649a5fc194aa9
AndriiKrupenko <krupenkoandrei1991@gmail.com>
2021-10-03 16:01:04 +0300

    tasks-2

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -11,9 +11,15 @@ const Tasks = () => {
                 <img src={editSvg} alt="Edit icon" />
             </h2>
             <div className="tasks__items">
-                <label className="checkbox">
-                    <input type="checkbox" />
-                </label>
+                <div className="tasks__items-row">
+                    <div className="checkbox">
+                        <input id="check" type="checkbox" />
+                        <label htmlFor="check"><svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg></label>    
+                    </div>
+                    <input value="ReactJS Hooks (useState, useReducer, useEffect и т.д.)" />
+                </div>
             </div>
         </div>
     )
